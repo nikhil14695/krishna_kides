636033cb81d81121b3ef5665c54ae259de474579
sojitra088-cmyk <146313621+nikhil14695@users.noreply.github.com>
2026-03-04 01:27:32 +0530

    Create herosectionimg_20260304012640.js

---
+++ .history/src/data/herosectionimg_20260304012640.js
@@ -0,0 +1,44 @@
+// import img1 from "../assets/studio_photos/1.webp";
+// import img2 from "../assets/studio_photos/2.webp";
+// import img3 from "../assets/studio_photos/3.webp";
+// import img4 from "../assets/studio_photos/4.webp";
+// import img5 from "../assets/studio_photos/5.webp";
+// import img6 from "../assets/studio_photos/6.webp";
+// import img7 from "../assets/studio_photos/7.webp";
+// import img8 from "../assets/studio_photos/8.webp";
+// import img9 from "../assets/studio_photos/9.webp";
+// import img10 from "../assets/studio_photos/10.webp";
+// import img11 from "../assets/studio_photos/13.webp";
+// import img12 from "../assets/studio_photos/12.webp";
+
+import img1 from "/studio_photos/1.webp";
+import img2 from "/studio_photos/2.webp";
+import img3 from "/studio_photos/3.webp";
+import img4 from "/studio_photos/4.webp";
+import img5 from "/studio_photos/5.webp";
+import img6 from "/studio_photos/6.webp";
+import img7 from "/studio_photos/7.webp";
+import img8 from "/studio_photos/8.webp";
+import img9 from "/studio_photos/9.webp";
+import img10 from "/studio_photos/10.webp";
+import img11 from "/studio_photos/13.webp";
+import img12 from "/studio_photos/12.webp";
+export const images = [
+  "https://drive.google.com/thumbnail?id=1OxrBCxEyOXIAor5HscxRLyJ3Zd-C60_n&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1REr1WXO6LCB9x_p6_AexLwBA4ZE1eR58&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1VGQDhdGPiX7IH4-CdVs3PpTMDBl4-r5q&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1bCREzJZUEmLFUxl4aJJ4Om5FJ8me81L3&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1g1dekGfoDH9_dJTKPNBSZDWtC9f3f5NV&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1hPOIEXxIch9SJ4Join-hUb5Ei5FyNihI&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1iyMOonBhrM-PfAqOTXKVIlCbEaCblUeq&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1lVJjj7zYTh-_vKOMYCHiKxYHQuSkrNJi&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1nfx_5A65r9x2fGNeUQRDD2FdHSJuFv0Y&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1qoVwpCdmJIJvbgs6vtYnUL4PlMylUsgO&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1tX7EXis4Dcnvj1jBk7UBZaWOm_lA1p0R&sz=w1000",
+  "https://drive.google.com/thumbnail?id=1w9reKGEBdK8WuD5RkW6w1lIl_NJhFgcH&sz=w1000"
+];
+
+// export const images = [
+//   img1,img2,img3,img4,img5,img6,
+//   img7,img8,img9,img10,img11,img12
+// ];
