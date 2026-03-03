ec8038136b2d061db020ac834b222efdc60189b8
sojitra088-cmyk <146313621+nikhil14695@users.noreply.github.com>
2026-03-04 01:52:19 +0530

    Update herosectionimg.js

--- src/data/herosectionimg.js
+++ src/data/herosectionimg.js
@@ -24,21 +24,21 @@ import img10 from "/studio_photos/10.webp";
 import img11 from "/studio_photos/13.webp";
 import img12 from "/studio_photos/12.webp";
 export const images = [
-  "https://drive.google.com/thumbnail?id=1OxrBCxEyOXIAor5HscxRLyJ3Zd-C60_n&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1REr1WXO6LCB9x_p6_AexLwBA4ZE1eR58&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1VGQDhdGPiX7IH4-CdVs3PpTMDBl4-r5q&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1bCREzJZUEmLFUxl4aJJ4Om5FJ8me81L3&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1g1dekGfoDH9_dJTKPNBSZDWtC9f3f5NV&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1hPOIEXxIch9SJ4Join-hUb5Ei5FyNihI&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1iyMOonBhrM-PfAqOTXKVIlCbEaCblUeq&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1lVJjj7zYTh-_vKOMYCHiKxYHQuSkrNJi&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1nfx_5A65r9x2fGNeUQRDD2FdHSJuFv0Y&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1qoVwpCdmJIJvbgs6vtYnUL4PlMylUsgO&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1tX7EXis4Dcnvj1jBk7UBZaWOm_lA1p0R&sz=w1000",
-  "https://drive.google.com/thumbnail?id=1w9reKGEBdK8WuD5RkW6w1lIl_NJhFgcH&sz=w1000"
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/1.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/10.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/13.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/2.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/3.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/4.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/5.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/6.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/7.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/8.webp",
+  "https://psgoihplrbdqxfkvxqwp.supabase.co/storage/v1/object/public/hero%20section/9.webp"
 ];
 
 // export const images = [
 //   img1,img2,img3,img4,img5,img6,
 //   img7,img8,img9,img10,img11,img12
 // ];
+
